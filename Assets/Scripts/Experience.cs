57739c7e129f5b0430b5e42b4a056d3296b40aa7
Dragonia428 <settineri.john@gmail.com>
2016-07-03 19:23:38 -0400

    New Game uploaded

---
+++ Assets/Scripts/Experience.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Experience {
+    public int experience;
+    public int tonextlevel;
+    static float experienceprogress;
+    Experience(){ experience = 0; }
+    Experience(int exp)
+    { this.experience = exp; }
+    private void SetExp(int exp){this.experience = exp;}
+    public int GetExp() { return experience; }
+    public float GetExperienceProgress()
+    {   return (float)(experience) / (float)(tonextlevel);}
+    public static Experience operator +(Experience exp1, Experience exp2)
+    {
+        return new Experience(exp1.experience + exp2.experience);
+    }
+    public override string ToString()
+    {
+        return this.experience.ToString();
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Level {
+    static int level = 1;
+    // Use this for initialization
+    public static void LevelUp()
+    {
+        level++;
+    }
+    private bool ifUnderOne(int level)
+    {
+        if (level < 1) return true;
+        else return false;
+    }
+}
